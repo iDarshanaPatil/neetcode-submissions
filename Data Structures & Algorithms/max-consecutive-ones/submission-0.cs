c3fa5d8a1bf861d7068932455dc5631903d590e1
Darshana Patil <idarshanapatil@icloud.com>
2026-08-14 00:12:45 -0700

    Add: max-consecutive-ones - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int FindMaxConsecutiveOnes(int[] nums) {
+         int count=0;
+         int maxCount=0;
+        for(int i=0; i<nums.Length;i++)
+        {
+            if(nums[i]== 1)
+            {
+                count += 1;
+                maxCount= Math.Max(maxCount, count);
+                
+            }
+            else
+            {
+                count = 0;
+            }
+        }
+        return maxCount;
+        
+    }
+ }
