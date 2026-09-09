9742a2455047577ce5efa0acf2ac27919c797605
Darshana Patil <idarshanapatil@icloud.com>
2026-09-08 22:33:00 -0700

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable=10
+print(type(variable))
+variable= 2.80
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Swis"
+print(type(variable))
+variable = [1,4,5]
+print(type(variable))
